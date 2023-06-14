9eee93ed00a3d4a44db7612f207d2bed88f25174
James <85806019+James-Discord@users.noreply.github.com>
2023-06-14 17:00:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
+
+let sessionToken = '';
+
+function getSessionToken() {
+  try {
+    sessionToken = fs.readFileSync('sessionToken.txt', 'utf8').trim();
+  } catch (error) {
+    console.error('Error reading session token:', error);
+  }
+}
+
+getSessionToken();
 
 app.get('/answer', async (req, res) => {
   try {
@@ -12,13 +25,7 @@ app.get('/answer', async (req, res) => {
       return;
     }
 
-    const sessionToken = await getSessionToken();
-    if (!sessionToken) {
-      res.status(500).json({ error: 'Session token not found' });
-      return;
-    }
-
-    const _bard = new bardapi(sessionToken);
+    const _bard = new bardapi();
     const answer = await _bard.getAnswer(question);
     res.json({ answer });
   } catch (error) {
@@ -44,7 +51,7 @@ app.all('/ask-gpt', async (req, res) => {
       ]
     }, {
       headers: {
-        'Authorization': 'Bearer pk-***[OUR_API_KEY]***',
+        'Authorization': `Bearer ${sessionToken}`,
         'Content-Type': 'application/json'
       }
     });
@@ -63,39 +70,13 @@ app.all('/ask-gpt', async (req, res) => {
     }
 
     const answer = choices[0].message.content;
-
-    if (!answer) {
-      console.error('No answer received from GPT API');
-      res.status(500).json({ error: 'An error occurred while fetching the answer from GPT' });
-      return;
-    }
-
     res.json({ answer });
   } catch (error) {
-    console.error('Error fetching answer from GPT:', error.message);
+    console.error('Error fetching answer from GPT:', error);
     res.status(500).json({ error: 'An error occurred while fetching the answer from GPT' });
   }
 });
 
-function getSessionToken() {
-  return new Promise((resolve, reject) => {
-    fs.readFile('session_token.log', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const sessionToken = data.trim().split('\n')[0];
-        resolve(sessionToken || null);
-      }
-    });
-  });
-}
-
-// Error handler middleware
-app.use((err, req, res, next) => {
-  console.error('Error:', err);
-  res.status(500).json({ error: 'Internal server error' });
-});
-
 app.listen(3000, () => {
-  console.log('Express API server is running on port 3000');
+  console.log('Server listening on port 3000');
 });
